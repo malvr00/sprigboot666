16ad2b077520d4e5f591b8d24b46563c86c64cf3
조용일 <yedn1128@naver.com>
2024-02-12 14:22:50 +0900

    Add: init

---
+++ src/main/java/com/szs/yongil/YongilApplication.java
@@ -0,0 +1,13 @@
+package com.szs.yongil;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class YongilApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(YongilApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/szs/yongil/YongilApplicationTests.java
@@ -0,0 +1,13 @@
+package com.szs.yongil;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class YongilApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
